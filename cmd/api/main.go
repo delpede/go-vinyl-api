e51540bbd3ed7107bb979db002a52b884dc99fc5
Peter Larsen <delpede@gmail.com>
2026-05-15 11:13:00 +0200

    Moved main.go to cmd folder

---
+++ cmd/api/main.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/delpede/go-vinyl-api/internal/api/generated"
+	"github.com/delpede/go-vinyl-api/internal/db"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Server struct {
+	DB *pgxpool.Pool
+}
+
+func (s *Server) GetRecords(c *gin.Context, params generated.GetRecordsParams) {
+	rows, err := s.DB.Query(
+		context.Background(),
+		`
+		SELECT id, title, artist, created_at, updated_at
+		FROM records
+		ORDER BY created_at DESC
+		`,
+	)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	fmt.Println(rows)
+	defer rows.Close()
+
+	records := []generated.Record{}
+	for rows.Next() {
+		var record generated.Record
+
+		err := rows.Scan(
+			&record.Id,
+			&record.Title,
+			&record.Artist,
+			&record.CreatedAt,
+			&record.UpdatedAt,
+		)
+
+		if err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+		records = append(records, record)
+	}
+
+	c.JSON(200, records)
+}
+
+func (s *Server) PostRecords(c *gin.Context) {
+	var req generated.CreateRecordRequest
+
+	if err := c.BindJSON(&req); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	var record generated.Record
+	err := s.DB.QueryRow(
+		context.Background(),
+		`
+		INSERT INTO records (title, artist)
+		VALUES ($1, $2)
+		RETURNING id, title, artist, created_at, updated_at
+		`,
+		req.Title,
+		req.Artist,
+	).Scan(
+		&record.Id,
+		&record.Title,
+		&record.Artist,
+		&record.CreatedAt,
+		&record.UpdatedAt,
+	)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(201, record)
+}
+
+func (s *Server) GetRecordsId(c *gin.Context, id generated.RecordId) {
+	var record generated.Record
+
+	err := s.DB.QueryRow(
+		context.Background(),
+		`
+		SELECT id, title, artist, created_at, updated_at
+		FROM records
+		WHERE id = $1
+		`,
+		id,
+	).Scan(
+		&record.Id,
+		&record.Title,
+		&record.Artist,
+		&record.CreatedAt,
+		&record.UpdatedAt,
+	)
+
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			c.JSON(404, gin.H{"error": "record not found"})
+			return
+		}
+
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(200, record)
+}
+
+func (s *Server) PutRecordsId(c *gin.Context, id generated.RecordId) {
+	var req generated.UpdateRecordRequest
+
+	if err := c.BindJSON(&req); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	var record generated.Record
+	err := s.DB.QueryRow(
+		context.Background(),
+		`
+		UPDATE records
+		SET title = $1, artist = $2, updated_at = NOW()
+		WHERE id = $3
+		RETURNING id, title, artist, created_at, updated_at
+		`,
+		req.Title,
+		req.Artist,
+		id,
+	).Scan(
+		&record.Id,
+		&record.Title,
+		&record.Artist,
+		&record.CreatedAt,
+		&record.UpdatedAt,
+	)
+
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			c.JSON(404, gin.H{"error": "record not found"})
+			return
+		}
+
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(200, record)
+}
+
+func (s *Server) DeleteRecordsId(c *gin.Context, id generated.RecordId) {
+	commandTag, err := s.DB.Exec(
+		context.Background(),
+		`
+		DELETE FROM records
+		WHERE id = $1
+		`,
+		id,
+	)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	if commandTag.RowsAffected() == 0 {
+		c.JSON(404, gin.H{"error": "record not found"})
+		return
+	}
+
+	c.Status(204)
+}
+
+func main() {
+	router := gin.Default()
+
+	dbpool, err := db.New()
+	if err != nil {
+		panic(err)
+	}
+
+	server := &Server{
+		DB: dbpool,
+	}
+	generated.RegisterHandlers(router, server)
+
+	router.Run(":8080")
+}
